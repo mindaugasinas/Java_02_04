4cc0b54de2ffd20599961c715656f499ce1c6e4e
mindaugasinas <sinkeviciusm@gmail.com>
2018-01-30 15:30:33 +0200

    Ketvirta pamoka (baigtas projektas)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
-	// write your code here
+        System.out.println("Iveskite du skaicius");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+
+        System.out.println("Skaiciu suma: " + Suma(a, b));
+        System.out.println("Skaiciu skirtumas: " + Skirtumas(a, b));
+    }
+
+    public static int Suma(int a, int b) {
+        return a + b;
+    }
+
+    public static int Skirtumas(int a, int b) {
+        return a - b;
     }
 }
